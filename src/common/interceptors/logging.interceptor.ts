0273f3cf896da169c4b0dfe14ab5c3534f8b6273
Changhun Lee <ch.lee@danbicorp.com>
2022-08-24 14:32:10 +0900

    refactor: http logger interceptor 개선 및 exception filter 제거

--- src/common/interceptors/logging.interceptor.ts
+++ src/common/interceptors/logging.interceptor.ts
@@ -1,47 +1,125 @@
-import { Logger } from '@nestjs/common';
+import { HttpException, HttpStatus, Logger } from '@nestjs/common';
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
 import { Request, Response } from 'express';
+import prettyjson, { RendererOptions } from 'prettyjson';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
-	intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+	private request: Request;
+	private response: Response;
+	private prettyJsonConfig: RendererOptions = {
+		keysColor: 'rainbow',
+		dashColor: 'magenta',
+		stringColor: 'white',
+		defaultIndentation: 4,
+	};
+
+	public intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
 		const ctx = context.switchToHttp();
-		const request = ctx.getRequest<Request>();
-		const response = ctx.getResponse<Response>();
+		this.request = ctx.getRequest<Request>();
+		this.response = ctx.getResponse<Response>();
 
-		const { originalUrl, method, params, query, body } = request;
-		const { statusCode: statusCodeResponse } = response;
+		this.loggingRequest();
 
-		const loggingRequest = () => {
-			Logger.log(`
-	/* --------------------------------- 📭 Request -------------------------------- */
-	path		: ${originalUrl} 
-	method		: ${method}
-	params		: ${JSON.stringify(params)} 
-	query		: ${JSON.stringify(query)} 
-	body		: ${JSON.stringify(body)} 
-			`);
-		};
-
-		loggingRequest();
-
-		const loggingResponse = (data) => {
-			Logger.log(`
-	/* --------------------------------- 📬 Response -------------------------------- */
-	statusCode	: ${statusCodeResponse} 
-	data		: ${data}
-			`);
-		};
-
-		next.handle().pipe(
+		// :timestamp :method :url
+		return next.handle().pipe(
 			tap({
-				next(value) {
-					console.log('asdfasdfasdf', value);
+				next: (value) => {
+					this.loggingResponse(value);
+				},
+				error: (err: Error) => {
+					this.loggingErrResponse(err);
 				},
 			}),
 		);
-		return next.handle().pipe(tap((data) => loggingResponse(data)));
+	}
+
+	private loggingRequest() {
+		const { originalUrl, method, params, query, body, headers } = this.request;
+		// Logger.log(`
+		// /* --------------------------------- 📭 Request -------------------------------- */
+		// headers		: ${{ headers }}
+		// path		: ${originalUrl}
+		// method		: ${method}
+		// params		: ${JSON.stringify(params)}
+		// query		: ${JSON.stringify(query)}
+		// body		: ${JSON.stringify(body)}
+		// 		`);
+
+		// Logger.log(`/* --------------------------------- 📭 Request -------------------------------- */`, {
+		// 	timestamp: new Date().toISOString(),
+		// 	originalUrl,
+		// 	method,
+		// 	params,
+		// 	query,
+		// 	body,
+		// });
+
+		Logger.log(
+			`/* --------------------------------- 📭 Request -------------------------------- */`,
+		);
+		Logger.log(
+			prettyjson.render(
+				{
+					timestamp: new Date().toISOString(),
+					originalUrl,
+					method,
+					params,
+					query,
+					body,
+					headers,
+				},
+				this.prettyJsonConfig,
+			),
+		);
+	}
+
+	private loggingResponse(data: any) {
+		const { statusCode } = this.response;
+
+		// Logger.log(`
+		// /* --------------------------------- 📬 Response -------------------------------- */
+		// statusCode	: ${statusCode}
+		// data		: ${data}
+		// 		`);
+
+		Logger.debug(
+			`/* --------------------------------- 📬 Response -------------------------------- */`,
+		);
+		Logger.log(
+			prettyjson.render(
+				{
+					timestamp: new Date().toISOString(),
+					statusCode,
+					data,
+				},
+				this.prettyJsonConfig,
+			),
+		);
+	}
+
+	private loggingErrResponse(error: Error) {
+		const statusCode =
+			error instanceof HttpException ? error.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
+		const message = error instanceof HttpException ? error.message : 'Internal server error';
+
+		const { url, method } = this.request;
+
+		const errorResponse = {
+			timestamp: new Date().toISOString(), // 에러 발생 시간
+			statusCode, // 에러 코드
+			path: url, // url 경로
+			method, // http method 정보
+			message, // 에러 메시지
+		};
+
+		// 에러 메시지 로그 출력
+		Logger.error(
+			prettyjson.render(errorResponse, this.prettyJsonConfig),
+			error.stack,
+			'ExceptionFilter',
+		);
 	}
 }
