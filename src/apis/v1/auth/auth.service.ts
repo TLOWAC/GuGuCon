24b7ee380b052299c8178cfb7953f7e12a3896fd
Changhun Lee <ch.lee@danbicorp.com>
2022-09-02 10:43:00 +0900

    refactor: login & register 로직 추가

--- src/apis/v1/auth/auth.service.ts
+++ src/apis/v1/auth/auth.service.ts
@@ -1,27 +1,89 @@
-import { Injectable } from '@nestjs/common';
+import {
+        BadRequestException,
+        Injectable,
+        Logger,
+        NotAcceptableException,
+        NotFoundException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import bcryptjs from 'bcryptjs';
+import { Auth, google } from 'googleapis';
 
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { UserService } from '../user/user.service';
+import { loginUserDTO, RegisterUserDTO } from './dto';
 
 @Injectable()
 export class AuthService {
-        create(createAuthDto: CreateAuthDto) {
-                return 'This action adds a new auth';
+        private ouathClient: Auth.OAuth2Client;
+        private logger: Logger = new Logger(AuthService.name);
+
+        constructor(private userService: UserService, private jwtService: JwtService) {
+                const clientId = process.env.GOOGLE_CLIENT_ID;
+                const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+                this.ouathClient = new google.auth.OAuth2(clientId, clientSecret);
         }
 
-        findAll() {
-                return `This action returns all auth`;
+        async validateUser({ username, password }: { username: string; password: string }) {
+                try {
+                        const user = await this.userService.findUserByEmail(username);
+
+                        const isValid = bcryptjs.compareSync(user.password, password);
+                        if (!isValid) {
+                                throw new BadRequestException();
+                        }
+
+                        return user;
+                } catch (e) {
+                        this.logger.error(e);
+                }
         }
 
-        findOne(id: number) {
-                return `This action returns a #${id} auth`;
+        async register(registerUserDto: RegisterUserDTO) {
+                try {
+                        return await this.userService.createUser(registerUserDto);
+                } catch (error) {
+                        this.logger.error(error);
+                }
+
+                // this.userService.registerUserByEmail;
+                // this.userService.registerUserBySocial;
         }
 
-        update(id: number, updateAuthDto: UpdateAuthDto) {
-                return `This action updates a #${id} auth`;
+        async login(loginUserDTO: loginUserDTO) {
+                try {
+                        const { username, password } = loginUserDTO;
+                        const user = await this.userService.findOneUserByOption({ username });
+                        const hashedPassword = user.password;
+
+                        const isvalidPassword = bcryptjs.compare(hashedPassword, password);
+
+                        if (!user || !isvalidPassword) {
+                                throw new NotAcceptableException('no matching user exist');
+                        }
+
+                        // generate jwt access token
+                        const payload = { username: user.username, sub: user.id };
+                        const jwtAccessToken = this.jwtService.sign(payload);
+
+                        return {
+                                access_token: jwtAccessToken,
+                        };
+                } catch (e) {
+                        this.logger.error(e);
+                }
         }
 
-        remove(id: number) {
-                return `This action removes a #${id} auth`;
-        }
+        // async loginGoogle(
+        //         token: string,
+        //         values: { userAgent: string; ipAddress: string },
+        // ): Promise<{ accessToken: string; refreshToken: string } | undefined> {
+        //         const googleAuthInfo = await this.ouathClient.getTokenInfo(token);
+        //         const user = await this.userService.findUserByEmail(googleAuthInfo.email);
+
+        //         if (user) {
+        //                 /// publish accessToken & refreshToken
+        //         }
+
+        //         return undefined;
+        // }
 }
