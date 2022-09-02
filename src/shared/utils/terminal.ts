0e0817a400fcb87fab67b6adb1eb5ad25778e9ed
Changhun Lee <ch.lee@danbicorp.com>
2022-09-02 11:14:36 +0900

    chore: uilts 폴더로 이동

---
+++ src/shared/utils/terminal.ts
@@ -0,0 +1,21 @@
+import gradient from 'gradient-string';
+
+export function bootTerminalPrint() {
+        const coolGradient = gradient('red', 'blue', 'green');
+
+        // TODO: env > author 로 변경.
+        const coolString = coolGradient(String.raw`
+
+  /$$$$$$$$ /$$        /$$$$$$  /$$      /$$  /$$$$$$   /$$$$$$ 
+ |__  $$__/| $$       /$$__  $$| $$  /$ | $$ /$$__  $$ /$$__  $$
+    | $$   | $$      | $$  \ $$| $$ /$$$| $$| $$  \ $$| $$  \__/
+    | $$   | $$      | $$  | $$| $$/$$ $$ $$| $$$$$$$$| $$      
+    | $$   | $$      | $$  | $$| $$$$_  $$$$| $$__  $$| $$      
+    | $$   | $$      | $$  | $$| $$$/ \  $$$| $$  | $$| $$    $$
+    | $$   | $$$$$$$$|  $$$$$$/| $$/   \  $$| $$  | $$|  $$$$$$/
+    |__/   |________/ \______/ |__/     \__/|__/  |__/ \______/     
+
+    Made By ChangHun Lee
+      `);
+        return coolString;
+}
