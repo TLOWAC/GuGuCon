5c6b9f7aca9e901a426a4630be2d7a30842a74e5
Changhun Lee <ch.lee@danbicorp.com>
2022-09-02 14:35:29 +0900

    chore: auth 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
-import { LoggingInterceptor } from '@common/interceptors';
 import { Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import { PassportModule } from '@nestjs/passport';
 
+import { LoggingInterceptor } from '@@shared/interceptors';
+
+import { AuthModule } from './apis/v1/auth/auth.module';
 import { UserModule } from './apis/v1/user/user.module';
 import { CoreModule } from './libs/core.module';
 
 @Module({
-        imports: [CoreModule, UserModule],
+        imports: [CoreModule, UserModule, AuthModule, PassportModule],
         controllers: [],
         providers: [
                 {
