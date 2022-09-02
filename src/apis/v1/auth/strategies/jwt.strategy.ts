eb1e28e44fe6d4b3c7ce23488a4131129322edf7
Changhun Lee <ch.lee@danbicorp.com>
2022-09-02 15:05:53 +0900

    feat(strategy): jwt strategy 기능 추가

--- src/apis/v1/auth/strategies/jwt.strategy.ts
+++ src/apis/v1/auth/strategies/jwt.strategy.ts
@@ -1,3 +1,27 @@
-export function template() {
-        return '';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { UserService } from 'apis/v1/user/user.service';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+type JWTPayload = {
+        username: string;
+        sub: number;
+};
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+        constructor(private userService: UserService) {
+                super({
+                        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+                        ignoreExpiration: false,
+                        secretOrKey: process.env.JWT_SECRET,
+                });
+        }
+        async validate(payload: JWTPayload) {
+                const { username } = payload;
+                const user = await this.userService.findUserByEmail({ username });
+                if (!user) {
+                        throw new UnauthorizedException('Invalid Token');
+                }
+                return { userId: user.id, username: user.username };
+        }
 }
