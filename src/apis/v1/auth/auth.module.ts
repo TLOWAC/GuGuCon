7383a47bd7327af0a49b6aa3e7821f0ee908f567
Changhun Lee <ch.lee@danbicorp.com>
2022-09-02 15:17:37 +0900

    refactor(module): auth > Auth Strategy, PassportModule 설정

--- src/apis/v1/auth/auth.module.ts
+++ src/apis/v1/auth/auth.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
+import { UserModule } from '../user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
         controllers: [AuthController],
-        providers: [AuthService],
+        providers: [AuthService, JwtStrategy, LocalStrategy],
+        imports: [
+                UserModule,
+                JwtModule.register({
+                        secret: process.env.JWT_SECRET,
+                        signOptions: { expiresIn: '60s' },
+                }),
+                PassportModule,
+        ],
+        exports: [JwtStrategy],
 })
 export class AuthModule {}
