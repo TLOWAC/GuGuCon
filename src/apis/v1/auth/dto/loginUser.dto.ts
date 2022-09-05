24ce4e26cb0544e3e30718bdec6491383581f42b
chlee-tlowac <leechanghun55@gmail.com>
2022-09-05 14:29:51 +0900

    feat(dto): login dto 추가

---
+++ src/apis/v1/auth/dto/loginUser.dto.ts
@@ -0,0 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, Matches, MinLength } from 'class-validator';
+
+export class loginUserDTO {
+        @IsNotEmpty()
+        @IsString()
+        @MinLength(4)
+        @ApiProperty({ default: 'user@example.com' })
+        username: string;
+
+        @IsNotEmpty()
+        @IsString()
+        @MinLength(4)
+        @Matches(/^[a-zA-Z0-9]*$/, {
+                message: 'password only accepts english and number',
+        })
+        @ApiProperty({ default: 'qwer1234' })
+        password: string;
+}
