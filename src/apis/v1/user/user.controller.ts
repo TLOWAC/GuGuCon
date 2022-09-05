dff0d33ba99c16851b262667bbf69615a4bdee8c
chlee-tlowac <leechanghun55@gmail.com>
2022-09-05 14:22:42 +0900

    refactor: @ApiBearerAuth 데코 추가

--- src/apis/v1/user/user.controller.ts
+++ src/apis/v1/user/user.controller.ts
@@ -9,7 +9,7 @@ import {
         Request,
         UseGuards,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
 import { JwtAuthGuard } from '@@shared/guards/jwt-auth.guard';
 
@@ -18,10 +18,12 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { UserService } from './user.service';
 
 @ApiTags('User')
-@Controller('user')
+@Controller('api/v1/user')
 export class UserController {
         constructor(private readonly userService: UserService) {}
+
         @UseGuards(JwtAuthGuard)
+        @ApiBearerAuth()
         @Get('profile')
         getProfile(@Request() req) {
                 return req.user;
