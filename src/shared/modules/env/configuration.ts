64086e2031921a1ca43e0b86f6f0474889fc36be
chlee-tlowac <leechanghun55@gmail.com>
2022-09-26 17:28:28 +0900

    feat: oauth env 추가

--- src/shared/modules/env/configuration.ts
+++ src/shared/modules/env/configuration.ts
@@ -8,6 +8,9 @@ function configuration() {
                 db_password: process.env.DATABASE_PASSWORD,
                 db_name: process.env.DATABASE_NAME,
                 db_sync: process.env.DATABASE_SYNC,
+                oauth_google_client_id: process.env.OAUTH_GOOGLE_CLIENT_ID,
+                oauth_google_client_secret: process.env.OAUTH_GOOGLE_CLIENT_SECRET,
+                oauth_google_callback_url: process.env.OAUTH_CALLBACK_URL,
         };
 }
 
