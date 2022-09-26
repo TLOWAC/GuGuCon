7744666917a827f42215ef1c2e36804a1d9e4767
chlee-tlowac <leechanghun55@gmail.com>
2022-09-26 19:22:08 +0900

    refactor: ConfigModule 글로벌 설정 추가

--- src/apis/v1/auth/auth.module.ts
+++ src/apis/v1/auth/auth.module.ts
@@ -1,5 +1,5 @@
 import { Module } from "@nestjs/common";
-import { ConfigModule, ConfigService } from "@nestjs/config";
+import { ConfigService } from "@nestjs/config";
 import { JwtModule } from "@nestjs/jwt";
 import { PassportModule } from "@nestjs/passport";
 
@@ -16,7 +16,6 @@ import { LocalStrategy } from "./strategies/local.strategy";
         imports: [
                 UserModule,
                 JwtModule.registerAsync({
-                        imports: [ConfigModule],
                         inject: [ConfigService],
                         useFactory: (config: ConfigService) => ({
                                 secret: config.get<string>("JWT_SECRET"),
