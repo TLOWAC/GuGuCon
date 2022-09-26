f32ab3deaeb7e28d19e7076d88d99df473b0c6dc
chlee-tlowac <leechanghun55@gmail.com>
2022-09-26 17:28:05 +0900

    feat: oauth env 검증 로직 추가

--- src/shared/modules/env/validation.ts
+++ src/shared/modules/env/validation.ts
@@ -1,7 +1,7 @@
-import Joi from 'joi';
+import Joi from "joi";
 
 const validationSchema = Joi.object({
-        NODE_ENV: Joi.string().valid('local', 'dev', 'prod'),
+        NODE_ENV: Joi.string().valid("local", "dev", "prod"),
         PORT: Joi.number().default(3000),
         DB_HOST: Joi.string(),
         DB_PORT: Joi.string(),
@@ -9,6 +9,9 @@ const validationSchema = Joi.object({
         DB_USERNAME: Joi.string(),
         DB_NAME: Joi.string(),
         DB_SYNC: Joi.string(),
+        OAUTH_GOOGLE_CLIENT_ID: Joi.string(),
+        OAUTH_GOOGLE_CLIENT_SECRET: Joi.string(),
+        OAUTH_CALLBACK_URL: Joi.string(),
 });
 
 export { validationSchema };
