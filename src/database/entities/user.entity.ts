256ce7636d70488179c17b23c527a793b76afbb1
chlee-tlowac <leechanghun55@gmail.com>
2022-09-29 19:34:35 +0900

    refactor(entity): coreEntity 를 상속받아 사용하도록 수정

--- src/database/entities/user.entity.ts
+++ src/database/entities/user.entity.ts
@@ -1,15 +1,14 @@
 import { ApiProperty } from "@nestjs/swagger";
 import bcryptjs from "bcryptjs";
-import { classToPlain, classToPlainFromExist, Exclude } from "class-transformer";
+import { Exclude } from "class-transformer";
 import { IsNotEmpty, IsString, Matches, MinLength } from "class-validator";
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import crypto from "crypto";
+import { BeforeInsert, BeforeUpdate, Column, Entity } from "typeorm";
+
+import { CoreEntity } from "./@core.entity";
 
 @Entity()
-export class User {
-        @ApiProperty({ description: "id" })
-        @PrimaryGeneratedColumn()
-        id: number;
-
+export class User extends CoreEntity {
         @ApiProperty({ description: "이름" })
         @Column()
         firstName: string;
@@ -52,14 +51,28 @@ export class User {
         @Exclude()
         salt: string;
 
-        // https://stackoverflow.com/questions/50360101/how-to-exclude-entity-field-from-returned-by-controller-json-nestjs-typeorm
-        // user 데이터 반환시 JSON 데이터에 몇몇 Column 들을 제외하고 반환하고 싶은 경우.
-        // module[class-transformer] 의 @Exclude() 를 사용하고 classToPlain 을 사용한다. ( @Exclude() 만 사용하는 방법도 있다. )
-        // toJSON() 함수를 만들어서 보다 명시적으로 사용하고자 하는것으로 지금은 추측 된다.
+        @BeforeInsert()
+        @BeforeUpdate()
+        hashPassword() {
+                const salt = bcryptjs.genSaltSync();
+                this.password = bcryptjs.hashSync(this.password, salt);
+                this.salt = salt;
+        }
 
         validatePassword(password: string) {
                 const hashPassword = bcryptjs.hashSync(password, this.salt);
                 const isValidPassowrd = bcryptjs.compare(hashPassword, password);
                 return isValidPassowrd;
+        }
+
+        // * 로그인 > 비밀번호 찾기 | 비밀번호 리셋
+        resetPassword() {
+                const genPwd = crypto
+                        .randomBytes(12)
+                        .toString("base64")
+                        .replace(/[^A-Za-z0-9]/g, "");
+
+                this.password = genPwd; // Entity[User].hashPassword() 로 인해서 DB 에 insert 될때 hash 된다.
+                return genPwd;
         }
 }
